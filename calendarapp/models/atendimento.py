cd3ca7dc8605bc6a0a100b37646f4a83a4d995b9
Jackson Cavalcante <jackson.silva@grupoexp.com>
2023-01-24 14:55:44 -0300

    Conectando o banco de dados no front-end

---
+++ calendarapp/models/atendimento.py
@@ -0,0 +1,4 @@
+from django.db import models
+from calendarapp.models.db import *
+from accounts.models import User
+
--- eventcalendar/views.py
+++ eventcalendar/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-
+from calendarapp.models.db import *
 from calendarapp.models import Event
 
 
@@ -10,12 +10,35 @@ class DashboardView(LoginRequiredMixin, View):
     template_name = "calendarapp/dashboard.html"
 
     def get(self, request, *args, **kwargs):
-        events = Event.objects.get_all_events(user=request.user)
-        running_events = Event.objects.get_running_events(user=request.user)
-        latest_events = Event.objects.filter(user=request.user).order_by("-id")[:10]
+        Atendimentos = Atendimento.objects.all()
+        qtd_confirmados = 2
+        qtd_comcluidos = 20
+        qtd_reagendados = 5
+        tecnicos = 3
+    
         context = {
-            "total_event": events.count(),
-            "running_events": running_events,
-            "latest_events": latest_events,
+            "Confirmados": qtd_confirmados,
+            "Comcluidos": qtd_comcluidos,
+            "Atendimentos": Atendimentos,
+            "Reagendados": qtd_reagendados,
+            "Tecnicos": tecnicos,
+
         }
         return render(request, self.template_name, context)
+    
+    # class DashboardView(LoginRequiredMixin, View):
+    # login_url = "dashboard"
+    # template_name = "calendarapp/dashboard.html"
+
+    # def get(self, request, *args, **kwargs):
+    #     events = Event.objects.get_all_events(user=request.user)
+    #     running_events = Event.objects.get_running_events(user=request.user)
+    #     latest_events = Event.objects.filter(user=request.user).order_by("-id")[:10]
+    #     context = {
+    #         "total_event": events.count(),
+    #         "running_events": running_events,
+    #         "latest_events": latest_events,
+    #     }
+    #     return render(request, self.template_name, context)
+    
+
